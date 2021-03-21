fe8750da78faa2f7cb83c868e9a6efca54f63b92
Ömer Aran <omeraranofficial@gmail.com>
2021-03-22 02:00:42 +0300

    initialize commit

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+  public class PttManager :ISupplierService
+    {
+        private IApplicantService _applicantService;
+
+        public PttManager()
+        {
+        }
+
+        public PttManager(IApplicantService applicantService)
+        {
+            _applicantService = applicantService;
+        }
+        public void GiveMask(Person person)
+        {
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName+ " için maske verilebilir");
+            }else
+            {
+                Console.WriteLine(person.FirstName + " için maske VERİLEMEZ");
+            }
+        }
+    }
+}
---
+++ WorkSpace/Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace WorkSpace
+{
+  public  class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "Ömer";
+            person1.LastName = "Aran";
+            person1.NationalIdentity = 3535;
+            person1.DateOfBirthYear = 2001;
+            
+            
+            PttManager pttManager = new PttManager(new PersonManager() );
+            pttManager.GiveMask(person1);
+
+
+        }
+    }
+}
